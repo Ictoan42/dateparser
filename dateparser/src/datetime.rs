0bbed918ef89040904e0ed64792ef3699e9615a1
Ictoan42 <christopher@mlbennett.com>
2024-05-09 04:39:18 +0100

    add a whole buncha new test cases

--- dateparser/src/datetime.rs
+++ dateparser/src/datetime.rs
@@ -1211,6 +1211,10 @@ mod tests {
                 "May 27 02:45:27",
                 Utc.ymd(Utc::now().year(), 5, 27).and_hms(2, 45, 27),
             ),
+            (
+                "May 27, 02:45:27",
+                Utc.ymd(Utc::now().year(), 5, 27).and_hms(2, 45, 27),
+            ),
         ];
 
         for &(input, want) in test_cases.iter() {
@@ -1240,6 +1244,14 @@ mod tests {
             (
                 "September 17, 2012, 10:10:09",
                 Utc.ymd(2012, 9, 17).and_hms(10, 10, 9),
+            ),
+            (
+                "May 8 2009, 5:57:51 PM",
+                Utc.ymd(2009, 5, 8).and_hms(17, 57, 51),
+            ),
+            (
+                "September 17 2012 10:09am",
+                Utc.ymd(2012, 9, 17).and_hms(10, 9, 0),
             ),
         ];
 
@@ -1273,6 +1285,14 @@ mod tests {
             ),
             (
                 "September 17, 2012 at 10:09am PST",
+                Utc.ymd(2012, 9, 17).and_hms(18, 9, 0),
+            ),
+            (
+                "May 26 2021, 12:49 AM PDT",
+                Utc.ymd(2021, 5, 26).and_hms(7, 49, 0),
+            ),
+            (
+                "September 17 2012 at 10:09am PST",
                 Utc.ymd(2012, 9, 17).and_hms(18, 9, 0),
             ),
         ];
@@ -1317,6 +1337,14 @@ mod tests {
                 "October 7, 1970",
                 Utc.ymd(1970, 10, 7).and_time(Utc::now().time()),
             ),
+            (
+                "oct. 7 70",
+                Utc.ymd(1970, 10, 7).and_time(Utc::now().time()),
+            ),
+            (
+                "October 7 1970",
+                Utc.ymd(1970, 10, 7).and_time(Utc::now().time()),
+            ),
         ];
 
         for &(input, want) in test_cases.iter() {
@@ -1350,6 +1378,11 @@ mod tests {
                 "14 May 2019 19:11:40.164",
                 Utc.ymd(2019, 5, 14).and_hms_milli(19, 11, 40, 164),
             ),
+            (
+                "12 Feb, 2006, 19:17",
+                Utc.ymd(2006, 2, 12).and_hms(19, 17, 0),
+            ),
+            ("12 Feb, 2006 19:17", Utc.ymd(2006, 2, 12).and_hms(19, 17, 0)),
         ];
 
         for &(input, want) in test_cases.iter() {
@@ -1379,6 +1412,10 @@ mod tests {
             ),
             (
                 "1 July 2013",
+                Utc.ymd(2013, 7, 1).and_time(Utc::now().time()),
+            ),
+            (
+                "1 July, 2013",
                 Utc.ymd(2013, 7, 1).and_time(Utc::now().time()),
             ),
         ];
